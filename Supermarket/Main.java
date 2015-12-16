df6df4509df61448ab39aa18e1cf82c05ff17798
iswinso <aswinson31757@jcss.us>
2015-12-16 13:17:03 -0500

    Created by Coding Ground Client

---
+++ Supermarket/Main.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        Customer randy = new Customer("Randy", 100.0)
+        Store.addSale(randy, 50)
+        
+        System.out.println(randy.getTotal)
+    }
